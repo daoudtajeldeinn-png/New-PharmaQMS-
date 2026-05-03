68d44695bbfabb975af9fa8b51102632e24c4785
Daoud Tajeldeinn Ahmed <daoudtajeldeinn@gmail.com>
2026-05-03 10:01:29 +0200

    fix: remove duplicate BrowserRouter, App already has HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
@@ -12,8 +11,6 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
